359af018cd97768855c991bd5b219a3d9a8b6275
Thower56 <112575794+Thower56@users.noreply.github.com>
2023-08-31 12:10:25 -0400

    update

---
+++ www.tp3.com/serveur1/html/index.php
@@ -0,0 +1,17 @@
+<html>
+	<title>Tp3</title>
+	<h1>Mon site nginx numero 1 :D !!</h1>
+
+	<?php
+	$host = 'mariadb';
+	$user = 'root';
+	$pass = 'rootpassword';
+	$conn = new mysqli($host, $user, $pass);
+
+	if ($conn->connect_error) {
+    	   die("La connexion a échoué: " . $conn->connect_error);
+	}
+	echo "Connexion réussie à MariaDB!";
+	?>
+
+</html>
