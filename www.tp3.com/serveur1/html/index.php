640426d7c5e7a42196961787adcb60325df38b8c
root <root@DESKTOP-DD2ULRU>
2023-09-04 21:45:57 -0400

    Update, Proxy error encore

--- www.tp3.com/serveur1/html/index.php
+++ www.tp3.com/serveur1/html/index.php
@@ -1,17 +1,18 @@
 <html>
-	<title>Tp3</title>
+	<title>Tp3 page 1</title>
 	<h1>Mon site nginx numero 1 :D !!</h1>
 
-	<?php
+	<?php 
 	$host = 'mariadb';
 	$user = 'root';
 	$pass = 'rootpassword';
 	$conn = new mysqli($host, $user, $pass);
 
 	if ($conn->connect_error) {
-    	   die("La connexion a échoué: " . $conn->connect_error);
-	}
+		die("La connexion a échoué: " . $conn->connect_error);
+	} 
 	echo "Connexion réussie à MariaDB!";
 	?>
+
 
 </html>
--- www.tp3.com/serveur2/html/index.php
+++ www.tp3.com/serveur2/html/index.php
@@ -1,14 +1,15 @@
 <html>
-	<title>Tp3</title>
+	<title>Tp3 page 2 !</title>
 	<h1>Mon site nginx numero 2 :3 !!</h1>
 	<?php 
 	$host = 'mariadb';
 	$user = 'root';
 	$pass = 'rootpassword';
 	$conn = new mysqli($host, $user, $pass);
+
 	if ($conn->connect_error) {
-    		die("La connexion a échoué: " . $conn->connect_error);
-	}	 
+		die("La connexion a échoué: " . $conn->connect_error);
+	} 
 	echo "Connexion réussie à MariaDB!";
 	?>
 </html>
